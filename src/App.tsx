4860fdb6d4ca31980825b19136ce4c1fa9407130
Shreyass48 <sgkulkarni48@gmail.com>
2024-07-27 21:50:16 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
-      <p className="bg-red-400">shreyas</p>
+      <p className="bg-red-400">
+        Click on the Vite and fdhufhuReact logos to learn more
+      </p>
+      <Button>Hello</Button>
     </>
   );
 }
